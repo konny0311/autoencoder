a8608a4a14c6e14d9727cb3127c659fb9580da09
konny0311 <konotorie@gmail.com>
2019-07-17 15:46:56 +0900

    add ipynb

--- simple_autoencoder.py
+++ simple_autoencoder.py
@@ -4,8 +4,17 @@ from keras.models import Model
 encoding_dim = 32
 
 input_img = Input(shape=(784,))
-encoded = Dense(encoding_dim, activation='relu')(input_img)
-decoded = Dense(784, activation='sigmoid')(encoded)
+# encoded = Dense(encoding_dim, activation='relu')(input_img)
+# decoded = Dense(784, activation='sigmoid')(encoded)
+
+DIM = 128
+encoded = Dense(DIM, activation='relu')(input_img)
+encoded = Dense(DIM/2, activation='relu')(encoded)
+encoded = Dense(DIM/4, activation='relu')(encoded)
+
+decoded = Dense(DIM/2, activation='relu')(encoded)
+decoded = Dense(DIM, activation='relu')(decoded)
+decoded = Dense(784, activation='sigmoid')(decoded)
 
 autoencoder = Model(input_img, decoded)
 
